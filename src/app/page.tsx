88dd458cad50eee54c5f02427870076e2ab61976
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2024-04-10 21:24:30 +0100

    feat: refactor page component to dynamically generate correct word in Game component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Game from './components/Game/Game';
+import { getRandomWord } from './constants/words';
 
 export default function Home() {
+  const randomWord = getRandomWord();
   return (
     <div className='flex flex-col items-center justify-center min-h-screen'>
-      <h1>Wordle</h1>
-      <Game correctWord='hello' />
+      <h1 className='text-4xl font-bold mb-4'>Wordle</h1>{' '}
+      <Game correctWord={randomWord.toUpperCase()} />
     </div>
   );
 }
