7acc178b3a338bb6879ca235654a2be7681614a6
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2024-04-10 18:07:57 +0100

    feat: refactor Game component to handle guesses and provide feedback
    - visual feedback on guessed letters
    - green (correct position)
    - yellow (correct letter in incorrect position)
    - gray (incorrect letter)

--- src/app/components/Game/Game.tsx
+++ src/app/components/Game/Game.tsx
@@ -2,11 +2,32 @@
 
 import { ChangeEvent, FormEvent, useState } from 'react';
 
-const Game = () => {
-  const correctWord = 'hello';
+interface GameProps {
+  correctWord: string;
+}
+
+interface GuessState {
+  guess: string;
+  feedback: ('correct' | 'incorrect' | 'invalid')[];
+}
+
+const Game = ({ correctWord }: GameProps) => {
   const [currentGuess, setCurrentGuess] = useState('');
-  const [guesses, setGuesses] = useState<string[]>([]);
+  const [guesses, setGuesses] = useState<GuessState[]>([]);
   const [isGameOver, setIsGameOver] = useState(false);
+
+  const getGuessFeedback = (guess: string) => {
+    const feedback = guess.split('').map((letter, index) => {
+      if (correctWord[index] === letter) {
+        return 'correct';
+      } else if (correctWord.includes(letter)) {
+        return 'incorrect';
+      } else {
+        return 'invalid';
+      }
+    });
+    return feedback;
+  };
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setCurrentGuess(e.target.value);
@@ -19,7 +40,8 @@ const Game = () => {
       return;
     }
 
-    const updatedGuesses = [...guesses, currentGuess];
+    const feedback = getGuessFeedback(currentGuess);
+    const updatedGuesses = [...guesses, { guess: currentGuess, feedback }];
     const gameJustEnded =
       currentGuess === correctWord || updatedGuesses.length === 6;
 
@@ -53,8 +75,36 @@ const Game = () => {
           Guess
         </button>
       </form>
-      {guesses.map((guess, index) => (
-        <p key={index}>{guess}</p>
+      {guesses.map((item, index) => (
+        <div key={index} className='flex justify-center gap-2 p-1'>
+          {item.guess.split('').map((letter, letterIndex) => {
+            const feedbackClass = item.feedback[letterIndex];
+            let bgColorClass = '';
+
+            switch (feedbackClass) {
+              case 'correct':
+                bgColorClass = 'bg-green-500';
+                break;
+              case 'incorrect':
+                bgColorClass = 'bg-yellow-500';
+                break;
+              case 'invalid':
+                bgColorClass = 'bg-gray-500';
+                break;
+              default:
+                bgColorClass = 'bg-white';
+            }
+
+            return (
+              <span
+                key={letterIndex}
+                className={`p-4 ${bgColorClass} text-white`}
+              >
+                {letter}
+              </span>
+            );
+          })}
+        </div>
       ))}
       {isGameOver && (
         <div>
