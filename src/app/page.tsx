e9c5ac2bbc6be1fc80d301576bd3745c34b53124
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2024-04-11 08:09:39 +0100

    feat: add Toggle component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Game from './components/Game/Game';
+import Toggle from './components/Toggle/Toggle';
 import { getRandomWord } from './constants/words';
 
 export default function Home() {
   const randomWord = getRandomWord();
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen'>
-      <h1 className='text-4xl font-bold mb-4'>Wordle</h1>
+    <div className='flex flex-col items-center justify-center min-h-screen gap-4'>
+      <div className='flex justify-between items-center gap-4'>
+        <h1 className='text-4xl font-bold'>Wordle</h1>
+        <Toggle />
+      </div>
       <Game correctWord={randomWord.toUpperCase()} />
     </div>
   );
